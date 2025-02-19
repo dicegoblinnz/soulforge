3fbdf9ecfc1dc7d6a3abf75745f96b511f500a05
EonZeNx <kf4open@pm.me>
2025-02-19 15:49:46 +1300

    chore: renamed repo and project

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/soulforge_example",
+  basePath: "/soulforge",
 
   /**
    * Disable server-based image optimization. Next.js does not support
